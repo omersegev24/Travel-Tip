22b06019b6ee41252e513d0e6187f3993dc43629
omer segev <omer@MacBook-pro.local>
2020-02-19 13:43:37 +0200

    add map api key

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -41,7 +41,7 @@ function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
     const API_KEY = ''; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
-    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDuhEbi5g29ytt3d8IDAh7HHPJEzvA2Qmw`;
     elGoogleApi.async = true;
     document.body.append(elGoogleApi);
 
