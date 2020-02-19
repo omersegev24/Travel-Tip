9d2e69d926324acbd00228fa4a51e13b009ccd7b
omer segev <omer@MacBook-pro.local>
2020-02-19 22:58:36 +0200

    url fix

--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -9,7 +9,12 @@ const KEY = '296b1122b96bcab669d0beb757487874';
 
 
 function getWeather(pos) {
-    var prmWeather = axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${pos.latitude}&lon=${pos.longitude}&units=metric&appid=${KEY}`)
+    if (location.protocol === 'http:') {
+        var url = `http://api.openweathermap.org/data/2.5/weather?lat=${pos.latitude}&lon=${pos.longitude}&units=metric&appid=${KEY}`;
+     } else {
+        url = `https://api.openweathermap.org/data/2.5/weather?lat=${pos.latitude}&lon=${pos.longitude}&units=metric&appid=${KEY}`;
+     }
+    var prmWeather = axios.get(url)
         .then(res => {
             return {
                 address: res.data.name,
