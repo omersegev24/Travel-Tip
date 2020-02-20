960198b73456375434a96c92281b82c97765340f
omer segev <omer@MacBook-pro.local>
2020-02-20 09:55:52 +0200

    ...

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ console.log('Main!');
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
 import { weatherService } from './services/weather.service.js'
-import { utilsService } from './services/util.service.js';
 
 
 window.onload = () => {
@@ -21,21 +20,15 @@ window.onload = () => {
 }
 
 
-// function renderLocDetailsUrl() {
-//     var locDetailsTxt = utilsService.getParameterByName('name')
-//     if (locDetailsTxt) {
-//         document.querySelector('.loc-details span').innerText = locDetailsTxt
-//     }
-// }
 
 document.querySelector('.my-loc-btn').addEventListener('click', (ev) => {
     locService.getPosition()
-        .then(pos => {
-            mapService.panTo(pos.coords.latitude, pos.coords.longitude);
-            mapService.addMarker({ lat: pos.coords.latitude, lng: pos.coords.longitude })
-            weatherService.getWeather(pos.coords)
-                .then(renderWeather)
-        })
+    .then(pos => {
+        mapService.panTo(pos.coords.latitude, pos.coords.longitude);
+        mapService.addMarker({ lat: pos.coords.latitude, lng: pos.coords.longitude })
+        weatherService.getWeather(pos.coords)
+        .then(renderWeather)
+    })
 })
 
 
@@ -43,36 +36,35 @@ document.querySelector('.go-btn').addEventListener('click', (ev) => {
     var locationTxt = document.querySelector('.location-input').value
     if (!locationTxt) return;
     locService.getLocByName(locationTxt)
-        .then(loc => {
-            mapService.panTo(loc.lat, loc.lng)
-            mapService.addMarker({ lat: loc.lat, lng: loc.lng })
-            renderLocDetails(loc)
-        })
-        .catch(err => {
-            console.log('missing city', err)
-        })
+    .then(loc => {
+        mapService.panTo(loc.lat, loc.lng)
+        mapService.addMarker({ lat: loc.lat, lng: loc.lng })
+        renderLocDetails(loc)
+    })
+    .catch(err => {
+        console.log('missing city', err)
+    })
 })
 
 document.querySelector('.copy-btn').addEventListener('click', () => {
-    // var url = document.querySelector('.copy-input').value
     var copyText = document.querySelector(".copy-input");
     copyText.select();
     copyText.setSelectionRange(0, 99999)
     document.execCommand("copy");
-
+    
 })
 
 function renderWeather(weather) {
-
+    
     var strHTML = `<div class="weather-card">
-                        <p class="head-line">Weather today</p>
-                        <img src="${weather.icon}" alt="">
-                        <p class="address">${weather.address}, ${weather.country}</p>
-                        <p class="temp">Temp: ${weather.temp}℃</p>
-                        <p class="wind">Wind speed: ${weather.wind} m/s</p>
-                        <p class="feels-like">Feels like: ${weather.feelsLike}℃</p>
-                        <p class="min-temp">Temperature from ${weather.tempMin}℃ to ${weather.tempMax}℃</p>
-                    </div>`;
+    <p class="head-line">Weather today</p>
+    <img src="${weather.icon}" alt="">
+    <p class="address">${weather.address}, ${weather.country}</p>
+    <p class="temp">Temp: ${weather.temp}℃</p>
+    <p class="wind">Wind speed: ${weather.wind} m/s</p>
+    <p class="feels-like">Feels like: ${weather.feelsLike}℃</p>
+    <p class="min-temp">Temperature from ${weather.tempMin}℃ to ${weather.tempMax}℃</p>
+    </div>`;
     document.querySelector('.weather-container').innerHTML = strHTML
 }
 
@@ -81,3 +73,10 @@ function renderLocDetails(loc) {
     document.querySelector('.copy-input').value = `https://omersegev24.github.io/Travel-Tip/index.html?lat=${loc.lat}&lng=${loc.lng}&name=${loc.address}`
     document.querySelector('.location-input').value = ''
 }
+
+// function renderLocDetailsUrl() {
+//     var locDetailsTxt = utilsService.getParameterByName('name')
+//     if (locDetailsTxt) {
+//         document.querySelector('.loc-details span').innerText = locDetailsTxt
+//     }
+// }
